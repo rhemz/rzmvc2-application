88b7dd664bad0525c74846b50755208e9e6d993c
Russ Zeien <russ@zeien.org>
2013-01-15 18:32:32 -0500

    sample app config initial commit

--- config/routes.php
+++ config/routes.php
@@ -6,8 +6,11 @@
 
 	Argument access:
 		$n   i.e. $1, $2, $3, etc
+
+	Ex: 
+		"/xx/%var/%var/%var"	=> '/test/index/$1/$2/$3',
 */
 
 $config['mappings'] = array(
-	"/xx/%var/%var/%var"					=> '/test/index/$1/$2/$3'
+
 );
